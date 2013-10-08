ee18ddd52d0b9f2928970f7f627e96ce67ec3694
Tom Denley <tom.denley@youdevise.com>
2013-10-08 15:22:51 +0100

    reorganise in preparation for dynamic content

--- src/main/java/com/timgroup/eureka/server/EurekaContainer.java
+++ src/main/java/com/timgroup/eureka/server/EurekaContainer.java
@@ -5,6 +5,7 @@ import org.simpleframework.http.Request;
 import org.simpleframework.http.Response;
 import org.simpleframework.http.core.Container;
 
+import com.timgroup.eureka.server.responders.DynamicResourceResponder;
 import com.timgroup.eureka.server.responders.StaticResourceResponder;
 
 public final class EurekaContainer implements Container {
@@ -13,9 +14,12 @@ public final class EurekaContainer implements Container {
         final String[] path = target.getPath().getSegments();
         
         if (path.length == 0) {
-            return new StaticResourceResponder("/content/welcome.html");
+            return new StaticResourceResponder("/content/index.html");
         }
-        
+        if ("api".equals(path[0])) {
+            return new StaticResourceResponder("/api/" + path[1]);
+//            return new DynamicResourceResponder(path[1]);
+        }
         return new StaticResourceResponder("/content/" + path[0]);
     }
 
---
+++ src/main/java/com/timgroup/eureka/server/responders/DynamicResourceResponder.java
@@ -0,0 +1,17 @@
+package com.timgroup.eureka.server.responders;
+
+import org.simpleframework.http.Request;
+import org.simpleframework.http.Response;
+import org.simpleframework.http.core.Container;
+
+public final class DynamicResourceResponder implements Container {
+
+    public DynamicResourceResponder(String resource) {
+    }
+
+    @Override
+    public void handle(Request req, Response resp) {
+        
+    }
+
+}
