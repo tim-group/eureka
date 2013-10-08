d5ec1c6a934bd2383a238eb129424a004b406767
Tom Denley <tom.denley@youdevise.com>
2013-10-08 12:14:42 +0100

    parse json data with gson/guava

--- src/main/java/eureka/CpuUsageFetcher.java
+++ src/main/java/eureka/CpuUsageFetcher.java
@@ -1,26 +1,40 @@
 package eureka;
 
+import static com.google.common.collect.Iterables.getLast;
+import static java.lang.String.format;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
-import java.util.Map;
+
+import com.google.common.collect.Iterables;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class CpuUsageFetcher {
 
     public static void main(String[] args) throws Exception {
         String serverName = "production-ideasfxapp-001_mgmt_pg_net_local";
-        String cpuIdle = "collectd." + serverName + ".cpu-0.cpu-idle";
+        String cpuIdleMetricName = "collectd." + serverName + ".cpu-?.cpu-idle";
         
-        String url = "https://metrics.timgroup.com/render?target=" + cpuIdle + "&format=json&from=-60s";
+        String url = "https://metrics.timgroup.com/render?target=" + cpuIdleMetricName + "&format=json&from=-60s";
+        String data = call(url);
         
-        
-        
-        System.out.println(call(url));
+        JsonParser parser = new JsonParser();
+        JsonArray targets = parser.parse(data).getAsJsonArray();
+        for (JsonElement target : targets) {
+            JsonObject targetObject = target.getAsJsonObject();
+            String targetName = targetObject.get("target").getAsString();
+            JsonArray datapoints = targetObject.get("datapoints").getAsJsonArray();
+            BigDecimal cpuIdle = Iterables.get(getLast(datapoints).getAsJsonArray(), 0).getAsBigDecimal();
+            System.out.println(format("%s, %s", targetName, cpuIdle));
+        }
     }
-    
 
     public static String call(String urlString) throws IOException {
         try {
