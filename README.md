74e6a93fdee1777cb7f0cac8b2ece8d8bca939cc
Francesco Gigli <francesco.gigli@timgroup.com>
2013-10-25 11:49:59 +0100

    Port number as optional argument

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ How to run locally
 
     gradle jar
     java -cp build/libs/eureka.jar com.timgroup.eureka.Eureka
+
+You can also specify a port number:
+
+    java -cp build/libs/eureka.jar com.timgroup.eureka.Eureka 12345
+
--- src/main/java/com/timgroup/eureka/Eureka.java
+++ src/main/java/com/timgroup/eureka/Eureka.java
@@ -7,7 +7,18 @@ import com.timgroup.eureka.server.EurekaServer;
 public final class Eureka {
 
     public static void main(String[] args) throws IOException {
-        new EurekaServer(9444).start();
+        int port = 9444;
+
+        if (args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        new EurekaServer(port).start();
     }
 
 }
